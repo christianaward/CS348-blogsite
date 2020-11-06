129ea7995d5df0d1c714a2fd7536caa98ff98a6f
christianaward <christian@wardmail.net>
2020-11-06 09:23:59 +0000

    Initial Commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Comment;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()
+        ->has(Post::factory()->count(3)
+            ->has(Comment::factory())->count(3))
+        ->create();
+        //Post::factory(12)->create();
+        //Comment::factory(24)->create();
+    }
+}
