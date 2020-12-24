20282fded0af8956a8444d736dd82c1bab1b0d5f
christianaward <christian@wardmail.net>
2020-12-24 12:26:47 +0000

    Implement comment front-end

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,7 +10,7 @@
                         <h5 class="card-title">
                             <img class="img-circle" src="{{$post->user->avatar}}" alt="User Profile Image">
                             {{ $post->user->name }}
-                            <button type="button" class="btn text-white" style="float:right;">Reply&nbsp;<i class="fas fa-comment"></i></button>
+                            <button type="button" class="btn text-white" data-toggle="modal" data-target="#commentModal" style="float:right;">Reply&nbsp;<i class="fas fa-comment"></i></button>
 
                         </h5>
                         <p class="card-text">
@@ -40,5 +40,39 @@
         -->
         </div>
         <div class="col-4"></div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="commentModal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content bg-secondary text-black">
+                <div class="modal-body">
+                    <div class="card bg-primary text-white" style="margin-top: 10px;">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <img class="img-circle" src="{{$post->user->avatar}}" alt="User Profile Image">
+                                {{ $post->user->name }}
+                            </h5>
+                            <p class="card-text">
+                                {{ $post->body }}
+                            </p>
+                        </div>
+                    </div>
+
+                    <form>
+                        <div class="card bg-info text-white" style="margin-left: 10px;">
+                            <div class="card-body">
+                                <h6 class="card-title">
+                                    <img class="img-circle" src="{{$comment->user->avatar}}" alt="User Profile Image">
+                                    {{ $comment->user->name }}
+                                </h6>
+                                <textarea class="form-control" id="commentText" placeholder="Your reply" rows="2" maxlength="80"></textarea>
+                                <button type="button" class="btn bg-primary text-white" style="float:right; margin-top:5px;">Comment&nbsp;<i class="fas fa-comment-dots"></i></button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
