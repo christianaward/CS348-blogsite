855826ad7920bccf2f4f91d3e6cd093c8f8b1f08
christianaward <christian@wardmail.net>
2020-12-24 13:06:17 +0000

    Implement create account front-end

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row h-100">
+        <div class="col-2"></div>
+        <div class="col-3">
+            <form>
+                <h4 class="text-white">Create a new account.</h4>
+
+                <div class="input-group" style="margin-bottom: 40px;">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text font-weight-bold">Username</span>
+                    </div>
+                    <input type="text" class="form-control" id="username" placeholder="Username"/>
+                </div>
+
+                <div class="input-group" style="margin-bottom: 40px;">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text font-weight-bold">Email</span>
+                    </div>
+                    <input type="text" class="form-control" id="email" placeholder="Email"/>
+                </div>
+
+                <div class="input-group" style="margin-bottom: 20px;">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text font-weight-bold">Password</span>
+                    </div>
+                    <input type="password" class="form-control" id="password" placeholder="Password"/>
+                </div>
+
+                <button type="button" class="btn btn-primary" style="float: right;">Signup&nbsp;<i class="fas fa-user-plus"></i></button>
+            </form>
+        </div>
+        <div class="col-7"></div>
+    </div>
+@endsection
