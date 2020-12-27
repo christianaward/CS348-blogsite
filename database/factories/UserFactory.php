f10032dc57de813bdf50b0ebed69953dfffd7771
christianaward <christian@wardmail.net>
2020-12-27 10:46:41 +0000

    Add display_name as value in User Model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,6 +27,7 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
+            'display_name' => $this->faker->unique()->userName,
             'avatar' => $avatars[rand(0,1)],
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
