eca8b3c80e81a647b225961db6f35dd1b6bd1eaa
christianaward <christian@wardmail.net>
2020-12-27 11:40:36 +0000

    Create form to submit user signup data

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -4,7 +4,8 @@
     <div class="row h-100">
         <div class="col-2"></div>
         <div class="col-3">
-            <form>
+            <form method="POST" action="{{ route('users.store')}} ">
+                @csrf
                 <h4 class="text-white">Create a new account.</h4>
 
                 <div class="input-group" style="margin-bottom: 40px;">
@@ -28,7 +29,7 @@
                     <input type="password" class="form-control" id="password" placeholder="Password"/>
                 </div>
 
-                <button type="button" class="btn btn-primary" style="float: right;">Sign up&nbsp;<i class="fas fa-user-plus"></i></button>
+                <button type="submit" class="btn btn-primary" style="float: right;">Sign up&nbsp;<i class="fas fa-user-plus"></i></button>
             </form>
         </div>
         <div class="col-7"></div>
