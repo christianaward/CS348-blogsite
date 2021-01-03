e884a7015bdbc32f023f6fad071370afb0be3b07
christianaward <christian@wardmail.net>
2021-01-03 17:00:41 +0000

    Adjust to use of params in Unsplash

--- app/Unsplash.php
+++ app/Unsplash.php
@@ -4,16 +4,16 @@ namespace App;
 
 class Unsplash
 {
-    private $accessKey;
+    public $accessKey;
     private $secretKey;
 
     public function __construct($accessKey, $secretKey){
-        $this->$accessKey = $accessKey;
-        $this->$secretKey = $secretKey;
+        $this->accessKey = $accessKey;
+        $this->secretKey = $secretKey;
     }
 
     public function Photo() {
-        $json = file_get_contents('https://api.unsplash.com/photos/random?client_id=zSfUUDuhlVmh5yyqp9D-0SvCAbLHM_1VBgo94Te369g');
+        $json = file_get_contents('https://api.unsplash.com/photos/random?query=dog&orientation=landscape&client_id=' . $this->accessKey);
         $obj = json_decode($json);
         return $obj->urls->regular;
     }
